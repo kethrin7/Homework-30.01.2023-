601b537588ae1777f95a07d546889510fa12e5be
kethrin <ketevaniburjanadze@gmail.com>
2023-01-29 14:18:53 +0300

    added exercise-1

---
+++ app.js
@@ -0,0 +1,14 @@
+// 1. Create a program that takes an array of numbers and returns the sum of all the numbers in the array. Use a for loop to iterate over the array and add each number to a total variable.
+
+const numbers = [20, 30, 65, 87, 67, 108];
+
+function sum(arr) {
+  let num = 0;
+  for (let i = 0; i < arr.length; i++) {
+    num += arr[i];
+  }
+
+  console.log(num);
+}
+
+sum(numbers);
